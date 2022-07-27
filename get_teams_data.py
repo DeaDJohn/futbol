869434b8fe0e0beebd43ef7524646b29adbd72eb
Juan Jose Fernández <juanjofb86@gmail.com>
2022-07-27 22:11:32 +0200

    continuar

--- get_teams_data.py
+++ get_teams_data.py
@@ -50,7 +50,7 @@ for comp_url in comps_url:
     for team in teams:
         team_name = team.find_all("td", {"class": "hauptlink"})[0].find("a").text.strip()
         team_image = team.find_all("td", {"class": "zentriert"})[0].find("img").get("src").replace("tiny", "head")
-        team_profile = "https://www.transfermarkt.es" + team.find_all("td", {"class": "hauptlink"})[0].find("a").get("href") + "plus/1"
+        team_profile = "https://www.transfermarkt.es" + team.find_all("td", {"class": "hauptlink"})[0].find("a").get("href").replace('startseite','kader') + "/plus/1"
         df_teams.loc[len(df_teams)] = [team_name, team_image, team_profile]
 print(df_teams)
 teams_saved = df_teams.to_sql("tab_teams", con=db_connection, if_exists="append", index=False, chunksize=1000)
@@ -62,7 +62,7 @@ df_teams = df_teams.reset_index()
 logging.info("Empezando a obtener los datos de los paises")
 
 country_column_names = ["country_name", "country_image"]
-# df_countries = pd.DataFrame(columns=country_column_names)
+df_countries = pd.DataFrame(columns=country_column_names)
 
 for index, row in df_teams.iterrows():
     logging.info("Equipo {}".format(row["team_name"]))
@@ -84,14 +84,19 @@ logging.info("Total paises guardados: {}".format(countries_saved))
 """
 df_teams = pd.read_sql("SELECT * FROM tab_teams", con=db_connection)
 df_teams = df_teams.reset_index()
+
+countries = pd.read_sql("SELECT * FROM tab_countries", con=db_connection)
+countries = countries.reset_index()
+
 logging.info("Empezando a obtener los datos de los jugadores")
-player_column_names = ["player_name", "player_birth", "player_age", "player_height", "player_pref_foot", "player_position", "player_sing_date", "player_end_contract", "player_market_value", "player_img", "player_country"]
+player_column_names = ["player_name", "player_birth", "player_age", "player_height", "player_pref_foot", "player_position", "player_sing_date", "player_end_contract", "player_market_value", "player_img", "player_country", 'id_team', 'id_country']
 df_players = pd.DataFrame(columns=player_column_names)
 
 
 for index, row in df_teams.iterrows():
     logging.info("Equipo {}".format(row["team_name"]))
     url = row["team_profile"]
+    id_team = row["id_team"]
     response_obj = requests.get(url, headers=headers)
     page_bs = BeautifulSoup(response_obj.content, 'html.parser')
     players = page_bs.find_all("table", {"class": "items"})[0].find_all("tr", {"class": re.compile(r"odd|even")})
@@ -100,7 +105,7 @@ for index, row in df_teams.iterrows():
         player_name = player.find("td", {"class": "posrela"}).find("table").find("tr").find("td").find("img").get("title")
         
         player_name = remove_accents(player_name)
-        player_position = player.find_all( "td", {"class": "posrela"})[2].find("table").find_all("tr")[1].find("td").text.strip()
+        player_position = player.find( "td", {"class": "posrela"}).find("table").find_all("tr")[1].find("td").text.strip()
         player_birth = player.find_all("td", {"class": "zentriert"})[1].text
         player_age = player_birth.split("(")[1].split(")")[0]
         player_birth = player_birth.split("(")[0]
@@ -109,16 +114,20 @@ for index, row in df_teams.iterrows():
         player_sing_date = player.find_all("td", {"class": "zentriert"})[5].text
         player_end_contract = player.find_all("td", {"class": "zentriert"})[7].text
         player_market_value = player.find("td", {"class": "rechts hauptlink"}).text
-        player_img = player.find("td", {"class": "posrela"})[2].find("table").find("tr").find("td").find("img").get("data-src")
-        player_country = player.find("td", {"class": "zentriert"})[2].find("img").get("title")
+        player_img = player.find("td", {"class": "posrela"}).find("table").find("tr").find("td").find("img").get("data-src")
+        player_country = player.find_all("td", {"class": "zentriert"})[2].find("img").get("title")
         logging.info("Jugador {} - {} - {}".format(player_name, player_position, player_end_contract))
-        # id_country = countries[player_country]
-        df_players.loc[len(df_players)] = [player_name, player_birth, player_age, player_height, player_foot, player_position, player_sing_date, player_end_contract, player_market_value, player_img, player_country]
-for column in df_players :
-    df_players.loc[((df_players[column].isnull()) | (df_players[column] == "") | (df_players[column] == "-")), column] = np.nan
+        id_country = countries.loc[countries['country_name'] == player_country]['id_country']
+        print(id_country)
+        df_players.loc[len(df_players)] = [player_name, player_birth, player_age, player_height, player_foot, player_position, player_sing_date, player_end_contract, player_market_value, player_img, player_country, id_team, id_country]
+        break
+    break
+#for column in df_players :
+ #   df_players.loc[( (df_players[column] == "") | (df_players[column] == "-")), column] = np.nan
 
 df_players['player_birth'] = df_players['player_birth'].astype('datetime64[ns]')
+df_players['player_sing_date'] = df_players['player_sing_date'].astype('datetime64[ns]')
 df_players['player_end_contract'] = df_players['player_end_contract'].astype('datetime64[ns]')
-
+print(df_players)
 player_saved = df_players.to_sql("tab_players", con=db_connection, if_exists="append", index=False, chunksize=1000)
 print(player.prettify())
