3704100dd25b0ce900a0bf3ce64a8e2b5a63d998
Juanjo Fernández <juanjofb86@gmail.com>
2023-05-01 00:31:10 +0200

    first functional state

--- get_stats_data.py
+++ get_stats_data.py
@@ -10,13 +10,20 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+import re
 import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-
+def slugify(s):
+  s = s.lower().strip()
+  s = re.sub(r'[^\w\s-]', '', s)
+  s = re.sub(r'[\s_-]+', '-', s)
+  s = re.sub(r'^-+|-+$', '', s)
+  return s
 
 logging.basicConfig(
     filename="log_stats.txt",
@@ -33,7 +40,7 @@ mysql_db = "futbol_bd"
 db_connection = create_engine(f'mysql+pymysql://{user}:{pwd}@{host}:3306/{mysql_db}')
 connection = db_connection.connect
 logging.info(db_connection)
-
+id_season = 1
 def normalize_data(df):
     df = df.replace(u"á", "a").replace(u"é", "e").replace(u"í", "i").replace(u"ó", "o").replace(u"ú", "u")
     return df
@@ -47,27 +54,27 @@ def reorder_columns(df, column, pos):
 #driver = webdriver.Chrome("/usr/local/bin/chromedriver")
 
 # Player (stats)
-stats = ['player', "nationality", "position", "squad", "age",  "games", "games_start", "minutes", "goals", "assists"]
+stats = ["player", "id_player","player_slug", "id_season", "nationality", "position", "squad", "age",  "games", "games_start", "minutes", "goals", "assists"]
 
 # Goalkeepers (stats)
-keepers = ["player","nationality","position","age","gk_games","gk_games_starts","gk_minutes","minutes_90s","gk_goals_against","gk_goals_against_per90","gk_shots_on_target_against","gk_saves","gk_save_pct","gk_wins","gk_ties","gk_losses","gk_clean_sheets","gk_clean_sheets_pct","gk_pens_att","gk_pens_allowed","gk_pens_saved","gk_pens_missed","gk_pens_save_pct","matches"]
+keepers = ["player", "id_player","player_slug", "id_season","nationality","position","age","gk_games","gk_games_starts","gk_minutes","minutes_90s","gk_goals_against","gk_goals_against_per90","gk_shots_on_target_against","gk_saves","gk_save_pct","gk_wins","gk_ties","gk_losses","gk_clean_sheets","gk_clean_sheets_pct","gk_pens_att","gk_pens_allowed","gk_pens_saved","gk_pens_missed","gk_pens_save_pct","matches"]
 
-keepersadv = ["player","nationality","position","age","minutes_90s","gk_goals_against","gk_pens_allowed","gk_free_kick_goals_against","gk_corner_kick_goals_against","gk_own_goals_against","gk_psxg","gk_psnpxg_per_shot_on_target_against","gk_psxg_net","gk_psxg_net_per90","gk_passes_completed_launched","gk_passes_launched","gk_passes_pct_launched","gk_passes","gk_passes_throws","gk_pct_passes_launched","gk_passes_length_avg","gk_goal_kicks","gk_pct_goal_kicks_launched","gk_goal_kick_length_avg","gk_crosses","gk_crosses_stopped","gk_crosses_stopped_pct","gk_def_actions_outside_pen_area","gk_def_actions_outside_pen_area_per90","gk_avg_distance_def_actions","matches"]
+keepersadv = ["player", "id_player","player_slug", "id_season","nationality","position","age","minutes_90s","gk_goals_against","gk_pens_allowed","gk_free_kick_goals_against","gk_corner_kick_goals_against","gk_own_goals_against","gk_psxg","gk_psnpxg_per_shot_on_target_against","gk_psxg_net","gk_psxg_net_per90","gk_passes_completed_launched","gk_passes_launched","gk_passes_pct_launched","gk_passes","gk_passes_throws","gk_pct_passes_launched","gk_passes_length_avg","gk_goal_kicks","gk_pct_goal_kicks_launched","gk_goal_kick_length_avg","gk_crosses","gk_crosses_stopped","gk_crosses_stopped_pct","gk_def_actions_outside_pen_area","gk_def_actions_outside_pen_area_per90","gk_avg_distance_def_actions","matches"]
 keepersadv2 = []
 
-shooting = ["player","nationality","position","age","minutes_90s","goals","shots_total","shots_on_target","shots_on_target_pct","shots_total_per90","shots_on_target_per90","goals_per_shot","goals_per_shot_on_target","average_shot_distance","shots_free_kicks","pens_made","pens_att","xg","npxg","npxg_per_shot","xg_net","npxg_net","matches"]
+shooting = ["player", "id_player","player_slug", "id_season","nationality","position","age","minutes_90s","goals","shots_total","shots_on_target","shots_on_target_pct","shots_total_per90","shots_on_target_per90","goals_per_shot","goals_per_shot_on_target","average_shot_distance","shots_free_kicks","pens_made","pens_att","xg","npxg","npxg_per_shot","xg_net","npxg_net","matches"]
 
-passing = ["player","nationality","position","age","minutes_90s","passes_completed","passes","passes_pct","passes_total_distance","passes_progressive_distance","passes_completed_short","passes_short","passes_pct_short","passes_completed_medium","passes_medium","passes_pct_medium","passes_completed_long","passes_long","passes_pct_long","assists","xa","xa_net","assisted_shots","passes_into_final_third","passes_into_penalty_area","crosses_into_penalty_area","progressive_passes","matches"]
+passing = ["player", "id_player","player_slug", "id_season","nationality","position","age","minutes_90s","passes_completed","passes","passes_pct","passes_total_distance","passes_progressive_distance","passes_completed_short","passes_short","passes_pct_short","passes_completed_medium","passes_medium","passes_pct_medium","passes_completed_long","passes_long","passes_pct_long","assists","xa","xa_net","assisted_shots","passes_into_final_third","passes_into_penalty_area","crosses_into_penalty_area","progressive_passes","matches"]
 
-passing_types2 = ["player","nationality","position","age","minutes_90s","passes","passes_live","passes_dead","passes_free_kicks","through_balls","passes_pressure","passes_switches","crosses","corner_kicks","corner_kicks_in","corner_kicks_out","corner_kicks_straight","passes_ground","passes_low","passes_high","passes_left_foot","passes_right_foot","passes_head","throw_ins","passes_other_body","passes_completed","passes_offsides","passes_oob","passes_intercepted","passes_blocked","matches"]
+passing_types2 = ["player", "id_player","player_slug", "id_season","nationality","position","age","minutes_90s","passes","passes_live","passes_dead","passes_free_kicks","through_balls","passes_pressure","passes_switches","crosses","corner_kicks","corner_kicks_in","corner_kicks_out","corner_kicks_straight","passes_ground","passes_low","passes_high","passes_left_foot","passes_right_foot","passes_head","throw_ins","passes_other_body","passes_completed","passes_offsides","passes_oob","passes_intercepted","passes_blocked","matches"]
 
-gca = ["player","nationality","position","age","minutes_90s","sca","sca_per90","sca_passes_live","sca_passes_dead","sca_dribbles","sca_shots","sca_fouled","sca_defense","gca","gca_per90","gca_passes_live","gca_passes_dead","gca_dribbles","gca_shots","gca_fouled","gca_defense","matches",]
+gca = ["player", "id_player","player_slug", "id_season","nationality","position","age","minutes_90s","sca","sca_per90","sca_passes_live","sca_passes_dead","sca_dribbles","sca_shots","sca_fouled","sca_defense","gca","gca_per90","gca_passes_live","gca_passes_dead","gca_dribbles","gca_shots","gca_fouled","gca_defense","matches",]
 
-defense = ["player","nationality","position","age","minutes_90s","tackles","tackles_won","tackles_def_3rd","tackles_mid_3rd","tackles_att_3rd","dribble_tackles","dribbles_vs","dribble_tackles_pct","dribbled_past","pressures","pressure_regains","pressure_regain_pct","pressures_def_3rd","pressures_mid_3rd","pressures_att_3rd","blocks","blocked_shots","blocked_shots_saves","blocked_passes","interceptions","tackles_interceptions","clearances","errors","matches"]
+defense = ["player", "id_player","player_slug", "id_season","nationality","position","age","minutes_90s","tackles","tackles_won","tackles_def_3rd","tackles_mid_3rd","tackles_att_3rd","dribble_tackles","dribbles_vs","dribble_tackles_pct","dribbled_past","pressures","pressure_regains","pressure_regain_pct","pressures_def_3rd","pressures_mid_3rd","pressures_att_3rd","blocks","blocked_shots","blocked_shots_saves","blocked_passes","interceptions","tackles_interceptions","clearances","errors","matches"]
 
-possession = ["player","nationality","position","age","minutes_90s","touches","touches_def_pen_area","touches_def_3rd","touches_mid_3rd","touches_att_3rd","touches_att_pen_area","touches_live_ball","dribbles_completed","dribbles","dribbles_completed_pct","players_dribbled_past","nutmegs","carries","carry_distance","carry_progressive_distance","progressive_carries","carries_into_final_third","carries_into_penalty_area","miscontrols","dispossessed","pass_targets","passes_received","passes_received_pct","progressive_passes_received","matches"]
+possession = ["player", "id_player","player_slug", "id_season","nationality","position","age","minutes_90s","touches","touches_def_pen_area","touches_def_3rd","touches_mid_3rd","touches_att_3rd","touches_att_pen_area","touches_live_ball","dribbles_completed","dribbles","dribbles_completed_pct","players_dribbled_past","nutmegs","carries","carry_distance","carry_progressive_distance","progressive_carries","carries_into_final_third","carries_into_penalty_area","miscontrols","dispossessed","pass_targets","passes_received","passes_received_pct","progressive_passes_received","matches"]
 
-misc = ['player', 'nationality', 'position', 'age', 'minutes_90s', 'cards_yellow', 'cards_red', 'cards_yellow_red', 'fouls', 'fouled', 'offsides', 'crosses', 'interceptions', 'tackles_won', 'pens_won', 'pens_conceded', 'own_goals', 'ball_recoveries', 'aerials_won', 'aerials_lost', 'aerials_won_pct', 'matches']
+misc = ["player", "id_player","player_slug", "id_season", 'nationality', 'position', 'age', 'minutes_90s', 'cards_yellow', 'cards_red', 'cards_yellow_red', 'fouls', 'fouled', 'offsides', 'crosses', 'interceptions', 'tackles_won', 'pens_won', 'pens_conceded', 'own_goals', 'ball_recoveries', 'aerials_won', 'aerials_lost', 'aerials_won_pct', 'matches']
 
 
 def get_tables(url):
@@ -92,32 +99,49 @@ def get_tables(url):
 
 def get_frame( features, player_table, is_outfield_player):
     pre_df_player = dict()
+    pre_df_player_not_found = dict()
     features_wanted_player = features
     row_player = player_table.find_all("tr")
+    player_added = False
+    pre_df_player['id_player'] = []
+    pre_df_player['id_season'] = []
+    pre_df_player['player_slug'] = []
+    pre_df_player_not_found['player_name'] = []
+    pre_df_player_not_found['player_slug'] = []
     for row in row_player:
         if row.find(class_='thead'):
             continue
         logging.info(row)
+        player_added = False
         if(row.find("th", {"scope": "row"}) != None ):
             if( row.find("td", {"data-stat": 'position'}) != None) :
+                pre_df_player['id_season'].append(id_season)
                 if((is_outfield_player == True) & (row.find("td", {"data-stat": 'position'}).text.strip() != "GK")):
                     for f in features_wanted_player:
                         cell = row.find("td", {"data-stat": f})
                         if cell:
                             a = cell.text.strip().encode()
                             text = a .decode('utf-8')
+                            if(text== 'David Ozoh'):
+                                text = text
                             if (text == ""):
-                                text = 0
+                                text = None
                             if (text == "-"):
-                                text = 0
+                                text = None
                             if( f == 'matches' and text == 'Partidos'):
                                 continue
                             if (f == 'nationality'):
                                 # Dejar el codigo internacional
-                                text = text[-3:]
+                                if(text == None):
+                                    text = None
+                                else:
+                                    text = text[-3:]
                             if (f == 'age'):
-                                # Dejar solo la edad.
-                                text = text[0:2]
+                                if(text == ""):
+                                    text = None
+                                else:
+                                    # Dejar solo la edad.
+                                    text = text[0:2]
                             if ((f != "player") & (f != 'nationality') & (f != "position") & (f != "squad") & (f != "age") & (f != "birth_year") ) :
                                 if( type(text) == str ):
                                     text = float(text.replace(",", ""))
@@ -125,8 +149,25 @@ def get_frame( features, player_table, is_outfield_player):
                                 pre_df_player[f].append(text)
                             else:
                                 pre_df_player[f] = [text]
-                            
-                            pre_df_player['id_season'] = 1
+                                pre_df_player_not_found
+                            if( f == 'player'):
+                                pre_df_player['player_slug'].append(slugify(text))
+                                if isinstance(text, bytes):
+                                    text = text.decode('utf-8') # Si es una cadena de bytes, convertirla a texto
+                                if( player_added == False ):
+                                    df_player = pd.read_sql('''SELECT id_player FROM tab_player where player_slug = "'''+ slugify(text) +'''"''', con=db_connection)
+ 
+                                    if(df_player.size > 0):   
+                                        id_player = df_player.iloc[0]['id_player']
+                                        pre_df_player['id_player'].append(id_player)
+                                        
+                                    else:
+                                        pre_df_player['id_player'].append(1)
+                                        pre_df_player_not_found['player_name'].append(text)
+                                        pre_df_player_not_found['player_slug'].append(slugify(text))
+                                    
+                                    player_added = True
+
                 elif( (is_outfield_player == False) & (row.find("td", {"data-stat": 'position'}).text.strip() != "GK")):
                     for f in features_wanted_player:
                         cell = row.find("td", {"data-stat": f})
@@ -141,7 +182,9 @@ def get_frame( features, player_table, is_outfield_player):
                                 pre_df_player[f].append(text)
                             else:
                                 pre_df_player[f] = [text]
+        print(pre_df_player_not_found)
     df_player = pd.DataFrame.from_dict(pre_df_player)
+    logging.info(pre_df_player_not_found)
     return df_player
             
 
@@ -197,13 +240,16 @@ def get_goalkeepers_of_web():
     df_keeper = pd.concat([df_kpr_premier_league, df_kpr_la_liga, df_kpr_serie_a, df_kpr_bundesliga, df_kpr_bundesliga], ignore_index=True)
     df_keeper.drop_duplicates(subset=['player'], keep='last', inplace=True, ignore_index=True)
 
-#try:
-#    #this will fail if there is a new column
-player_saved = df_out_premier_league.to_sql(name='tab_stats_player', con=db_connection, if_exists = 'append', index=False)
-#except:
-#    data = pd.read_sql('SELECT * FROM tab_stats_player', db_connection)
-#    df_out_premier_league2 = pd.concat([data,df_out_premier_league])
-#    player_saved = df_out_premier_league2.to_sql(name='tab_stats_player', con=db_connection, if_exists = 'replace', index=False)
+try:
+    #this will fail if there is a new column
+    #df_out_premier_league = get_outfield_data('https://fbref.com/es/comps/9/', '/Estadisticas-de-Premier-League/')
+    df_out_players = get_players_of_web()
+    player_saved = df_out_players.to_sql(name='tab_stats_player', con=db_connection, if_exists = 'append', index=False, chunksize=1000)
+except:
+    data = pd.read_sql('SELECT * FROM tab_stats_player', db_connection)
+    df_out_premier_league = get_outfield_data('https://fbref.com/es/comps/9/', '/Estadisticas-de-Premier-League/')
+    df_out_premier_league2 = pd.concat([data,df_out_premier_league])
+    player_saved = df_out_premier_league.to_sql(name='tab_stats_player', con=db_connection, if_exists='append', index=False, chunksize=1000)
 #
 logging.info("Total jugadores guardados: {}".format(player_saved))
 print(df_out_premier_league)
--- get_teams_data.py
+++ get_teams_data.py
@@ -32,9 +32,15 @@ logging.info(db_connection)
 def remove_accents(text):
     return unicodedata.normalize('NFD', text).encode('ascii', 'ignore').decode("utf-8")
 
-
+def slugify(s):
+  s = s.lower().strip()
+  s = re.sub(r'[^\w\s-]', '', s)
+  s = re.sub(r'[\s_-]+', '-', s)
+  s = re.sub(r'^-+|-+$', '', s)
+  return s
 def get_teams_from_web() :
     '''Obtener los equipos de las 5 principales ligas de futbol.'''
+
     comps_url = [
         'https://www.transfermarkt.es/laliga/startseite/wettbewerb/ES1',
         'https://www.transfermarkt.es/premier-league/startseite/wettbewerb/GB1',
@@ -42,20 +48,23 @@ def get_teams_from_web() :
         'https://www.transfermarkt.es/1-bundesliga/startseite/wettbewerb/L1',
         'https://www.transfermarkt.es/ligue-1/startseite/wettbewerb/FR1'
     ]
-
+    df_leagues = pd.read_sql("SELECT * FROM tab_leagues", con=db_connection)
+    df_leagues = df_leagues.reset_index()
     logging.info("Empezando a obtener los datos de los equipos")
-    columns_names = ["team_name", "team_image", "team_profile"]
+    columns_names = ["team_name", "team_image", "team_profile", "id_league"]
     df_teams = pd.DataFrame(columns = columns_names)
 
-    for comp_url in comps_url:
+    for index, row  in df_leagues.iterrows():
+        comp_url = row["league_transfermarket"]
         response_obj = requests.get(comp_url, headers=headers)
         page_bs = BeautifulSoup(response_obj.content, 'html.parser')
         teams = page_bs.find_all("table", {"class": "items"})[0].find_all("tr", {"class": re.compile(r"odd|even")})
         for team in teams:
+            id_league = row["id_league"]
             team_name = team.find_all("td", {"class": "hauptlink"})[0].find("a").text.strip()
             team_image = team.find_all("td", {"class": "zentriert"})[0].find("img").get("src").replace("tiny", "head")
             team_profile = "https://www.transfermarkt.es" + team.find_all("td", {"class": "hauptlink"})[0].find("a").get("href").replace('startseite','kader') + "/plus/1"
-            df_teams.loc[len(df_teams)] = [team_name, team_image, team_profile]
+            df_teams.loc[len(df_teams)] = [team_name, team_image, team_profile, id_league]
     print(df_teams)
     teams_saved = df_teams.to_sql("tab_teams", con=db_connection, if_exists="append", index=False, chunksize=1000)
     logging.info("Total equipos guardados: {}".format(teams_saved))
@@ -68,7 +77,7 @@ def get_countries_from_web():
     df_teams = df_teams.reset_index()
     logging.info("Empezando a obtener los datos de los paises")
 
-    country_column_names = ["name", "image"]
+    country_column_names = ["name_country", "name_country_img"]
     df_countries = pd.DataFrame(columns=country_column_names)
 
     for index, row in df_teams.iterrows():
@@ -80,11 +89,11 @@ def get_countries_from_web():
         
         for player in players :
             player_image_element = player.find_all("td", {"class": "zentriert"})[2].find("img")
-            player_country = player_image_element.get("title")
-            if not any( df_countries['name'] == player_country) :
-                logging.info("País encontrado {}".format(player_country))
-                player_country_img = player_image_element.get("src").replace("verysmall", "head")
-                df_countries.loc[len(df_countries)] = [player_country, player_country_img]
+            name_country = player_image_element.get("title")
+            if not any( df_countries['name_country'] == name_country) :
+                logging.info("País encontrado {}".format(name_country))
+                name_country_img = player_image_element.get("src").replace("verysmall", "head")
+                df_countries.loc[len(df_countries)] = [name_country, name_country_img]
     countries_saved = df_countries.to_sql("tab_countries", con=db_connection, if_exists="append", index=False,  chunksize=1000)
     logging.info("Total paises guardados: {}".format(countries_saved))
 
@@ -97,14 +106,16 @@ def get_players_from_web():
     #countries = countries.reset_index()
 
     logging.info("Empezando a obtener los datos de los jugadores")
-    player_column_names = ["name", "birth", "age", "height", "pref_foot", "position", "sing_date", "end_contract", "market_value", "image", "country", 'id_team', 'id_country']
+    player_column_names = ["player_name", "player_birth", "player_age", "player_height", "player_foot", "player_position", "player_sing_date", "player_end_contract", "player_market_value", "player_img", "player_country", 'id_team', 'id_country', 'id_league', 'player_slug', 'id_season']
     df_players = pd.DataFrame(columns=player_column_names)
 
 
     for index, row in df_teams.iterrows():
         logging.info("Equipo {}".format(row["team_name"]))
+        print("Equipo {}".format(row["team_name"]))
         url = row["team_profile"]
         id_team = row['id_team']
+        id_league = row['id_league']
         response_obj = requests.get(url, headers=headers)
         page_bs = BeautifulSoup(response_obj.content, 'html.parser')
         players = page_bs.find_all("table", {"class": "items"})[0].find_all("tr", {"class": re.compile(r"odd|even")})
@@ -112,7 +123,8 @@ def get_players_from_web():
         for player in players :
             player_name = player.find("td", {"class": "posrela"}).find("table").find("tr").find("td").find("img").get("title")
             
-            player_name = remove_accents(player_name)
+            player_name = player_name
+            player_slug = slugify(player_name)
             player_position = player.find( "td", {"class": "posrela"}).find("table").find_all("tr")[1].find("td").text.strip()
             player_birth = player.find_all("td", {"class": "zentriert"})[1].text
             player_age = player_birth.split("(")[1].split(")")[0]
@@ -125,19 +137,20 @@ def get_players_from_web():
             player_img = player.find("td", {"class": "posrela"}).find("table").find("tr").find("td").find("img").get("data-src")
             player_country = player.find_all("td", {"class": "zentriert"})[2].find("img").get("title")
             logging.info("Jugador {} - {} - {}".format(player_name, player_position, player_end_contract))
-            id_country = countries.loc[countries['name'] == player_country]['id_country'].values[0]
+            id_country = countries.loc[countries['name_country'] == player_country]['id_country'].values[0]
+            id_season = 1
             
-            df_players.loc[len(df_players)] = [player_name, player_birth, player_age, player_height, player_foot, player_position, player_sing_date, player_end_contract, player_market_value, player_img, player_country, id_team, id_country]
-            
+            df_players.loc[len(df_players)] = [player_name,player_birth, player_age, player_height, player_foot, player_position, player_sing_date, player_end_contract, player_market_value, player_img, player_country, id_team, id_country, id_league,player_slug, id_season]
+
     for column in df_players :
         df_players.loc[( (df_players[column] == " ") | (df_players[column] == "-")), column] = np.nan
 
-        df_players['birth'] = pd.to_datetime(df_players['birth'], infer_datetime_format=True, utc=True, errors='ignore')
-        df_players['sing_date'] = pd.to_datetime(df_players['sing_date'], infer_datetime_format=True, utc=True, errors='ignore')
-        df_players['end_contract'] = pd.to_datetime(df_players['end_contract'], infer_datetime_format=True, utc=True, errors='ignore')
+        df_players['player_birth'] = pd.to_datetime(df_players['player_birth'], infer_datetime_format=True, utc=True, errors='ignore')
+        df_players['player_sing_date'] = pd.to_datetime(df_players['player_sing_date'], infer_datetime_format=True, utc=True, errors='ignore')
+        df_players['player_end_contract'] = pd.to_datetime(df_players['player_end_contract'], infer_datetime_format=True, utc=True, errors='ignore')
 
 
-    player_saved = df_players.to_sql("tab_players", con=db_connection, if_exists="append", index=False, chunksize=1000)
+    player_saved = df_players.to_sql("tab_player", con=db_connection, if_exists="append", index=False, chunksize=1000)
     logging.info("Total jugadores guardados: {}".format(player_saved))
 
 
